9ad969efd9e49de991f70bfa9b18fc3f11ef1416
Anoop Kushwaha <anoopit41@gmail.com>
2026-09-11 16:10:23 +0530

    Revert "chore: add one more exception handling concepts"
    This reverts commit a579a60d00274a1a3d3cc9b9e715b798c153bd14.

--- errorHandling.py
+++ errorHandling.py
@@ -1,6 +1,3 @@
-'''
-1. 
-'''
 # num = int(input("Enter the number "))
 l=[2,3,4,5]
 try :
@@ -9,12 +6,3 @@ try :
     print("Error are not occur.")
 except:
     print("Please resolve error")
-
-'''
-2.
-'''
-
-try:
-    result = 10/0
-except ZeroDivisionError:
-    print("Cannot divide by zero.")

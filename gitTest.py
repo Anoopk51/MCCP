0bac9c110b6f6e2ecf1a9d0bece79aa99badf31e
Anoop Kushwaha <anoopit41@gmail.com>
2026-09-15 15:35:22 +0530

    Revert "chore: Restore gitTest.py from 3892b7a"
    This reverts commit a05065c3203904dd701ddbf7f4630ecd8a89d1f6.

--- gitTest.py
+++ gitTest.py
@@ -2,9 +2,3 @@ try:
     total =  3/0
 except ZeroDivisionError:
     print("Zero Divison error throw.")
-
-try:
-    num = int("Anoop Kushwaha")
-except ValueError:
-    print("Invalid number")
-print("Hi Anoop")

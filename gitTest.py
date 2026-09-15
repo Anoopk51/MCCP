a05065c3203904dd701ddbf7f4630ecd8a89d1f6
Anoop Kushwaha <anoopit41@gmail.com>
2026-09-15 15:33:40 +0530

    chore: Restore gitTest.py from 3892b7a

--- gitTest.py
+++ gitTest.py
@@ -2,3 +2,9 @@ try:
     total =  3/0
 except ZeroDivisionError:
     print("Zero Divison error throw.")
+
+try:
+    num = int("Anoop Kushwaha")
+except ValueError:
+    print("Invalid number")
+print("Hi Anoop")
